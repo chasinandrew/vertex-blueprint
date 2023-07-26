3c2f86d42dd9aa32627e1f84f10a503e7c37d402
Andrew Chasin <andrewchasin@google.com>
2023-07-26 17:49:36 -0400

    adding back provider explicit blocks

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -15,3 +15,12 @@ terraform {
 
   # }
 }
+  provider "google" {
+    project = var.gcp_project_id
+    region  = var.gcp_region
+  }
+
+  provider "google-beta" {
+    project = var.gcp_project_id
+    region  = var.gcp_region
+  }
