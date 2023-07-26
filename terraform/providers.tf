0b105f4e42b92d85a63405378fbda3a930da7638
Andrew Chasin <andrewchasin@google.com>
2023-07-26 15:18:20 -0400

    provider changes to support cloud

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -3,12 +3,24 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.9.0, < 5.0.0"
+      version = ">= 4.9.0, < 5.0.0"
     }
 
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 3.9.0, < 5.0.0"
+      version = ">= 4.9.0, < 5.0.0"
     }
+  }
+  cloud {
+
+  }
+  provider "google" {
+    project = var.gcp_project_id
+    region = var.gcp_region
+  }
+
+  provider "google-beta" {
+    project = var.gcp_project_id
+    region = var.gcp_region
   }
 }
